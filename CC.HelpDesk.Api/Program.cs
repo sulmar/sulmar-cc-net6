0d3bb3f1ae7712ad168e8e507809e105c3df47d8
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-11-22 21:04:26 +0100

    Dodanie debugowania

--- CC.HelpDesk.Api/Program.cs
+++ CC.HelpDesk.Api/Program.cs
@@ -1,4 +1,4 @@
-using  Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Builder;
 using CC.HelpDesk.Domain;
 using CC.HelpDesk.IRepositories;
 using CC.HelpDesk.InMemoryRepositories;
@@ -33,66 +33,68 @@ var app = builder.Build();
 app.MapGet("/", () => "Hello HelpDesk User!");
 
 // Wstrzykiwanie zaleznosci (Dependency Injection / IoC)
-app.MapGet("api/users", (IUserRepository userRepository) => 
+app.MapGet("api/users", (IUserRepository userRepository) =>
 {
 
-   var users = userRepository.GetAll();
+    var users = userRepository.GetAll();
 
-   return users;
-    
+    return users;
+
 });
 
 // ## Zastosowanie reguł (constraint)
-app.MapGet("api/users/{id:int:min(1)}", (int id, IUserRepository userRepository) => {
-   
-   var user = userRepository.Get(id);
-
-   if (user == null)
-      return Results.NotFound(); // 404 NotFound
-
-   return Results.Ok(user);   // 200 OK
-}).WithName("GetUserById");
-
-app.MapGet("api/users/{name:alpha}", (string name, IUserRepository userRepository)=>
+app.MapGet("api/users/{id:int:min(1)}", (int id, IUserRepository userRepository) =>
 {
-   var user = userRepository.GetByName(name);
+
+    var user = userRepository.Get(id);
 
     if (user == null)
-      return Results.NotFound(); // 404 NotFound
+        return Results.NotFound(); // 404 NotFound
 
-   return Results.Ok(user);
+    return Results.Ok(user);   // 200 OK
+}).WithName("GetUserById");
+
+app.MapGet("api/users/{name:alpha}", (string name, IUserRepository userRepository) =>
+{
+    var user = userRepository.GetByName(name);
+
+    if (user == null)
+        return Results.NotFound(); // 404 NotFound
+
+    return Results.Ok(user);
 });
 
-app.MapPost("api/users", (User user, IUserRepository userRepository) => {
-   
-   // TODO: dodać walidację 
+app.MapPost("api/users", (User user, IUserRepository userRepository) =>
+{
 
-   userRepository.Add(user);
+    // TODO: dodać walidację 
 
-   // Location: http://localhost:5000/api/users/4
-  
-  // zła praktyka
-  // return Results.Created($"http://localhost:5000/api/users/{user.Id}", user);
+    userRepository.Add(user);
 
-   // dobra praktyka
-   return Results.CreatedAtRoute("GetUserById", new { id = user.Id }, user);
+    // Location: http://localhost:5000/api/users/4
+
+    // zła praktyka
+    // return Results.Created($"http://localhost:5000/api/users/{user.Id}", user);
+
+    // dobra praktyka
+    return Results.CreatedAtRoute("GetUserById", new { id = user.Id }, user);
 
 });
 
 app.MapPut("api/users/{id}", (int id, User user, IUserRepository userRepository) =>
 {
-   if (id != user.Id)
-      return Results.BadRequest();
+    if (id != user.Id)
+        return Results.BadRequest();
 
-   // TODO: dodać walidację 
-   userRepository.Update(user);
+    // TODO: dodać walidację 
+    userRepository.Update(user);
 
-   return Results.NoContent();
+    return Results.NoContent();
 });
 
 app.MapDelete("api/users/{id}", (int id, IUserRepository userRepository) =>
 {
-   // TODO: dodac sprawdzenie czy uzytkownik istnieje
+    // TODO: dodac sprawdzenie czy uzytkownik istnieje
 
     userRepository.Remove(id);
 
