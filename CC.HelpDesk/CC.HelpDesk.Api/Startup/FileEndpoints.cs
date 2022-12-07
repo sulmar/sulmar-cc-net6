16638ebf55a0e1d656adc2827ba437b7239e6a26
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-12-07 12:24:05 +0100

    Refaktoring

---
+++ CC.HelpDesk/CC.HelpDesk.Api/Startup/FileEndpoints.cs
@@ -0,0 +1,35 @@
+﻿namespace CC.HelpDesk.Api;
+
+public static class FileEndpoints
+{
+    public static WebApplication MapFileEndpoints(this WebApplication app)
+    {
+        app.MapGet("upload", (HttpRequest request) =>
+        {
+            if (!request.HasFormContentType)
+            {
+                return Results.BadRequest();
+            }
+
+            var form = request.Form;
+            var file = form.Files["file"];
+
+            if (file is null)
+            {
+                return Results.BadRequest();
+            }
+
+            var uploads = Path.Combine("uploads", file.FileName);
+            using var uploadStream = file.OpenReadStream();
+            using var fileStream = File.OpenWrite(uploads);
+
+            uploadStream.CopyTo(fileStream);
+
+            return Results.NoContent();
+
+        });
+        
+
+        return app;
+    }
+}
--- CC.HelpDesk/CC.HelpDesk.Api/Startup/UserEndpoints.cs
+++ CC.HelpDesk/CC.HelpDesk.Api/Startup/UserEndpoints.cs
@@ -22,40 +22,6 @@ public static class Endpoints
     }
 }
 
-public static class FileEndpoints
-{
-    public static WebApplication MapFileEndpoints(this WebApplication app)
-    {
-        app.MapGet("upload", (HttpRequest request) =>
-        {
-            if (!request.HasFormContentType)
-            {
-                return Results.BadRequest();
-            }
-
-            var form = request.Form;
-            var file = form.Files["file"];
-
-            if (file is null)
-            {
-                return Results.BadRequest();
-            }
-
-            var uploads = Path.Combine("uploads", file.FileName);
-            using var uploadStream = file.OpenReadStream();
-            using var fileStream = File.OpenWrite(uploads);
-
-            uploadStream.CopyTo(fileStream);
-
-            return Results.NoContent();
-
-        });
-        
-
-        return app;
-    }
-}
-
 public static class UserEndpoints 
 {
 
